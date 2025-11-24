db968e3672f68ab756efb063478ebf2202a9a426
Bionicuss <erkinov.bobur.97@gmail.com>
2025-11-24 14:46:33 +0500

    Create hw-1.py

---
+++ lesson-1 /homework/hw-1.py
@@ -0,0 +1,28 @@
+
+1. 
+s = float(input("Введите сторону квадрата: "))
+perimeter_square = 4 * s
+area_square = s ** 2
+print(f"Периметр квадрата = {perimeter_square}")
+print(f"Площадь квадрата = {area_square}")
+
+2. 
+d = float(input("\nВведите диаметр окружности: "))
+circumference = math.pi * d
+print(f"Длина окружности = {circumference}")
+
+3. 
+a = float(input("\nВведите число a: "))
+b = float(input("Введите число b: "))
+mean_ab = (a + b) / 2
+print(f"Среднее арифметическое чисел a и b = {mean_ab}")
+
+4. 
+sum_ab = a + b
+prod_ab = a * b
+square_a = a ** 2
+square_b = b ** 2
+print(f"Сумма = {sum_ab}")
+print(f"Произведение = {prod_ab}")
+print(f"Квадрат a = {square_a}")
+print(f"Квадрат b = {square_b}")
